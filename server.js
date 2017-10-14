75b8b47b6628c23d07ec1456640c4224f780249b
kgangadhar <KarthikGangadhar@users.noreply.github.com>
2017-10-15 03:52:55 +0530

    initial commit
    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+var httpRequest = require("request");
+
+exports.schedule = function (callback) {
+    httpRequest.get({
+        url: "http://apecricket.herokuapp.com:80/api/matchCalendar",
+    }, function (err, resp, body) {
+        callback(body);
+    });
+};
+
+exports.cricket = function (callback) {
+    httpRequest.get({
+        url: "http://apecricket.herokuapp.com:80/api/cricket",
+    }, function (err, resp, body) {
+        callback(body);
+    });
+};
+
+exports.upcomingMatches = function (callback) {
+    httpRequest.get({
+        url: "http://apecricket.herokuapp.com:80/api/matches",
+    }, function (err, resp, body) {
+        callback(body);
+    });
+};
+
+exports.cricketScore = function (unique_id, callback) {
+    httpRequest.post({
+        url: "http://apecricket.herokuapp.com:80/api/cricketScore/",
+        form: { unique_id: unique_id }
+    }, function (err, resp, body) {
+        callback(body);
+    });
+}
+
+exports.playerStats = function (pid, callback) {
+    httpRequest.post({
+        url: "http://apecricket.herokuapp.com:80/api/playerStats/",
+        form: { pid: pid }
+    }, function (err, resp, body) {
+        callback(body);
+    });
+}
