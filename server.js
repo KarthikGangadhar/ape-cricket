5dc32f659181dfad2fd1d9c6ef724001eb53190b
karthik <karthikg1643@gmail.com>
2017-10-16 22:26:44 +0530

    added apiKey to each endpoint

--- server.js
+++ server.js
@@ -1,42 +1,46 @@
 var httpRequest = require("request");
+apiKey = ""
 
-exports.schedule = function (callback) {
+exports.schedule = function (api_key, callback) {
     httpRequest.get({
         url: "http://apecricket.herokuapp.com:80/api/matchCalendar",
+        form:{ apikey: apikey }
     }, function (err, resp, body) {
         callback(body);
     });
 };
 
-exports.cricket = function (callback) {
+exports.cricket = function (api_key, callback) {
     httpRequest.get({
         url: "http://apecricket.herokuapp.com:80/api/cricket",
+         form:{ apikey: apikey }
     }, function (err, resp, body) {
         callback(body);
     });
 };
 
-exports.upcomingMatches = function (callback) {
+exports.upcomingMatches = function (api_key, callback) {
     httpRequest.get({
         url: "http://apecricket.herokuapp.com:80/api/matches",
+        form:{ apikey: apikey }        
     }, function (err, resp, body) {
         callback(body);
     });
 };
 
-exports.cricketScore = function (unique_id, callback) {
+exports.cricketScore = function (api_key, unique_id, callback) {
     httpRequest.post({
         url: "http://apecricket.herokuapp.com:80/api/cricketScore",
-        form: { unique_id: unique_id }
+        form: { unique_id: unique_id, apikey: apikey }
     }, function (err, resp, body) {
         callback(body);
     });
 }
 
-exports.playerStats = function (pid, callback) {
+exports.playerStats = function (api_key, pid, callback) {
     httpRequest.post({
         url: "http://apecricket.herokuapp.com:80/api/playerStats",
-        form: { pid: pid }
+        form: { pid: pid, apikey: apikey }
     }, function (err, resp, body) {
         callback(body);
     });
