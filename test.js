85c3658e5d882423b912ed1ff10122adf738e75d
karthik <karthikg1643@gmail.com>
2018-05-10 23:54:27 +0530

    updated version and package.json

--- test.js
+++ test.js
@@ -9,15 +9,19 @@ var unique_id = "1123492";
 //     console.log(response);
 // });
 
-apecricket.schedule(api_key, function (response) {
-    console.log(response);
-});
+// apecricket.schedule(api_key, function (response) {
+//     console.log(response);
+// });
+
+// apecricket.upcomingMatches(api_key, (Response) =>{
+//     console.log(Response);
+// })
+
 // apecricket.fantasySummary(api_key, unique_id, function (response) {
 //     console.log(response);
 //     // response will be json data of mentioned unique_id
 // });
 
-// apecricket.fantasySquad(api_key, unique_id, function (response) {
-//     console.log(response);
-//     // response will be json data of mentioned unique_id
-// });
+// apecricket.playerStats(api_key,'41224',Response => console.log(Response));
+
+// apecricket.cricketScore(api_key, unique_id, Response => console.log(Response));
